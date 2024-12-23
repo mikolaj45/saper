4310a8d12ef6da8b5b7f81613a623686339ca183
PwAAstud <01197780@pw.edu.pl>
2024-12-23 14:24:44 +0100

    definicja scoreborda

--- scorebord.h
+++ scorebord.h
@@ -3,11 +3,21 @@
 
 #define NAME_LEN 11
 #define BORD_LEN 5
+#define save_file "scorebord.txt"
 
 typedef struct{
     int wynik[BORD_LEN+1];
     char gracz[BORD_LEN+1][NAME_LEN];
 } scorebord;
 
+scorebord* scorebord_load();
+
+void scorebord_free();
+
+void scorebord_add(scorebord* link, int new_score, char player_name[NAME_LEN]);
+
+void scorebord_save(scorebord* link);
+
+void scorebord_print(scorebord* link);
 
 #endif
