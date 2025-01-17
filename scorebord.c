6617263c9201b3a1b6d960f6f30bcb5891f32446
PwAAstud <01197780@pw.edu.pl>
2025-01-17 20:18:51 +0100

    zamana scoreborda na dynamiczy rozmiar

--- scorebord.c
+++ scorebord.c
@@ -9,16 +9,26 @@ scorebord* scorebord_load(){
     FILE *sorse = fopen(SAVE_FILE, "r");
 
     if(sorse == NULL){
+        retVal->n = 1;
+        retVal->gracz = malloc(NAME_LEN * sizeof(char));
+        retVal->wynik = malloc(sizeof(int));
         return retVal;
     }
 
-    while (
-        fscanf(sorse, "%d : ", &retVal->wynik[retVal->n]) > 0 &&
-        load_name(sorse, retVal->gracz[retVal->n]) != EOF
-    ){
-        retVal->n++;
+    int ilosc_wpisow;
+    if(fscanf(sorse, "%d", &ilosc_wpisow)==0){
+        fprintf(stderr, "[!] 1 zly format");
     }
-    // retVal->n++;
+    retVal->n = ilosc_wpisow+1;
+    retVal->gracz = malloc(retVal->n*NAME_LEN * sizeof(char));
+    retVal->wynik = malloc(retVal->n*sizeof(int));
+    int i=0;
+    while (
+        fscanf(sorse, "%d : ", &retVal->wynik[i]) > 0 &&
+        load_name(sorse, retVal->gracz+i*NAME_LEN) != EOF
+    ){
+        i++;
+    }
 
     fclose(sorse);
 
@@ -30,9 +40,9 @@ void scorebord_free(scorebord* link){
 }
 
 void scorebord_add(scorebord* link, int new_score, char player_name[NAME_LEN]){
-    link->wynik[link->n] = new_score;
-    strcpy( link->gracz[link->n], player_name);
-    link->n++;
+    link->wynik[link->n-1] = new_score;
+    strcpy( link->gracz+(link->n-1)*NAME_LEN, player_name);
+    // link->n++;
 
     char cbuf[NAME_LEN];
     int ibuf;
@@ -40,21 +50,22 @@ void scorebord_add(scorebord* link, int new_score, char player_name[NAME_LEN]){
         if(link->wynik[i] <= link->wynik[i-1]){
             break;
         }
-        strcpy(cbuf, link->gracz[i]);
+        strcpy(cbuf, link->gracz+i*NAME_LEN);
         ibuf = link->wynik[i];
 
-        strcpy(link->gracz[i], link->gracz[i-1]);
+        strcpy(link->gracz+i*NAME_LEN, link->gracz+(i-1)*NAME_LEN);
         link->wynik[i] = link->wynik[i-1];
 
-        strcpy(link->gracz[i-1], cbuf);
+        strcpy(link->gracz+(i-1)*NAME_LEN, cbuf);
         link->wynik[i-1] = ibuf;
     }
 }
 
 void scorebord_save(scorebord* link){
     FILE* to_save = fopen(SAVE_FILE, "w");
-    for(int i=0;i<link->n && i<BORD_LEN;i++){
-        fprintf(to_save, "%d : %s\n", link->wynik[i], link->gracz[i]);
+    fprintf(to_save,"%d\n", link->n);
+    for(int i=0;i<link->n;i++){
+        fprintf(to_save, "%d : %s\n", link->wynik[i], link->gracz+i*NAME_LEN);
     }
     scorebord_free(link);
 }
@@ -62,12 +73,12 @@ void scorebord_save(scorebord* link){
 void scorebord_print(scorebord* link){
     // printf("  %*c : punkty", NAME_LEN, "imie");
     for(int i=0; i<link->n && i<BORD_LEN; i++){
-        printf("%d. %*s : %d\n", i+1, NAME_LEN-1,link->gracz[i],link->wynik[i]);
+        printf("%d. %*s : %d\n", i+1, NAME_LEN-1,link->gracz+i*NAME_LEN,link->wynik[i]);
     }
 };
 
 // -1 -EOF ; 0 - koniec ; 1 - zostalo 
-int load_name(FILE * stream, char retVal[NAME_LEN]){
+int load_name(FILE * stream, char* retVal){
     int n;
     char c;
     int end = 1;
--- scorebord.h
+++ scorebord.h
@@ -8,8 +8,11 @@
 #include<stdio.h>
 
 typedef struct{
-    int wynik[BORD_LEN+1];
-    char gracz[BORD_LEN+1][NAME_LEN];
+    int* wynik;
+    // lista znakow skladajaca sie na slowa 
+    //tylko jeda gwiastka aby wszystko trzymac w jednym miejscu jako ze imie jest ograniczone do NAME_LEN
+    char* gracz;
+    // ilosc graczy w plik plus 1 na nowego gracza
     int n;
 } scorebord;
 
@@ -19,10 +22,12 @@ void scorebord_free(scorebord* link);
 
 void scorebord_add(scorebord* link, int new_score, char player_name[NAME_LEN]);
 
+// ! nie wywoluwac bez dodania nowego wyniku
 void scorebord_save(scorebord* link);
 
+// ! nie wywoluwac bez dodania nowego wyniku
 void scorebord_print(scorebord* link);
 
-int load_name(FILE * stream, char retVal[NAME_LEN]);
+int load_name(FILE * stream, char* retVal);
 
 #endif
